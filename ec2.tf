ba8fba652028cdb56dbe671c72f834a758535532
Carnell <59581329+carnells@users.noreply.github.com>
2022-02-11 09:41:13 -0500

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provide "aaws" {
+  region = "us-east-1"
+}
+
+
+resouce "aws_instance" "demo_dev_server" {
+  ami = "ami-02354e95b39ca8dec"
+  instance_type = "t2.mirco"
+  
+  tags = {
+    name = "demo_dev_server"
+ }
+}  
